163b3bf79780d56eeaba5469c5efa0d4f607af30
Martin Dubois <mdubois@kms-quebec.com>
2022-01-11 08:53:34 -0500

    Hello

---
+++ Hello/Hello.cpp
@@ -0,0 +1,18 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2022 KMS
+// License   http ://www.apache.org/licenses/LICENSE-2.0
+// Product   TWS - CPP
+// File      THREA/THREA.cpp
+
+// ===== C ==================================================================
+#include <stdio.h>
+
+// Entry point
+// //////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
