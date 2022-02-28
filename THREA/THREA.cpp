e7668ae44650df76daec63074fafbec90d9a44b5
Martin Dubois <mdubois@kms-quebec.com>
2022-02-28 16:13:09 -0500

    Fix license link

--- THREA/THREA.cpp
+++ THREA/THREA.cpp
@@ -1,7 +1,7 @@
 
 // Author    KMS - Martin Dubois, P. Eng.
 // Copyright (C) 2022 KMS
-// License   http ://www.apache.org/licenses/LICENSE-2.0
+// License   http://www.apache.org/licenses/LICENSE-2.0
 // Product   TWS - CPP
 // File      THREA/THREA.cpp
 
